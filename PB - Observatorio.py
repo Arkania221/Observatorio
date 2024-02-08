ed93eed39683fb254c80a7281ed6dcba6c4ba99e
Ángel Borrás <angeloki1963@gmail.com>
2024-02-08 08:23:45 +0100

    Inicializacion de variables

---
+++ PB - Observatorio.py
@@ -0,0 +1,38 @@
+# Pre: Se presupone que el programa dara inicio a principios de año y de cada semana
+
+# Comenzamos creando las variables que se crean necesarias
+
+
+Temperaturas = [14, 15, 17, 18, 13, 18, 18, 16, 22, 13, 17, 18, 12, 14, 13]
+
+Semana = ["Lunes", "Martes", "Miercoles", "Jueves", "Viernes", "Sabado", "Domingo"]
+
+Meses = ["Enero", "Febrero", "Marzo", "Mayo", "Abril", "Julio", "Junio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+
+Enero = 31
+
+Febrero = 29
+
+Marzo = 31
+
+Mayo = 30
+
+Abril = 31
+
+Julio = 30
+
+Junio = 31
+
+Agosto = 30
+
+Septiembre = 31
+
+Octubre = 30
+
+Noviembre = 31
+
+Diciembre = 30
+
+Inicio_Año = ( 1, "de", "Enero")
+
+for 
