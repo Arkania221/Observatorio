6dd2bb0b70d824e0b7656095eafd3eb486f3d337
Ángel Borrás <angeloki1963@gmail.com>
2024-02-15 06:41:49 +0100

    Añadiendo los prints generales

--- PB - Observatorio.py
+++ PB - Observatorio.py
@@ -3,17 +3,18 @@ Cuatro def's iniciales: RT, MJ, DF, FI
 """
 
 def RT():
-
+print("Escribe las temperaturas de la semana:")
 
 
 def MJ():
-
-
+print("No hay temperaturas registradas")
+print("Hasta hoy" + "la media ha estado de " + "grados")
 
 
 def DF():
-
+print("Hasta hoy" + "la diferencia maxima ha estado de " + "grados")
 
 
 
 def FI():
+print("")
